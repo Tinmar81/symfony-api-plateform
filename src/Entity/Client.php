f57410d8b209470195999842ad56e8e6ae4d463f
Martin Dhenu <>
2020-08-21 17:32:19 +0200

    Task: Booking and Clients unmapped in APIRessource

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -2,12 +2,10 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\ClientRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ApiResource()
  * @ORM\Entity(repositoryClass=ClientRepository::class)
  */
 class Client
